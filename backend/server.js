8e9fc0652369ee2de5a39882b781d1b39084a5cd
Rehman Banday <rehmanbanday5@gmail.com>
2026-08-04 21:14:14 +0500

    HDR Sports

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://hdr-sports.vercel.app",
+      "https://hdr-sports-admin.vercel.app/admin/login",
     ],
     credentials: true,
   }),
